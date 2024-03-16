fd87f48d62e9cfe9a3254b978110c0e82752bd71
Roma Rogozhkin <roma.rogozhkin22@gmail.com>
2024-03-16 13:32:17 +0200

    Initial Commit

---
+++ Headway_Player/Headway_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Headway_PlayerApp.swift
+//  Headway_Player
+//
+//  Created by Roma Rohozhkin on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Headway_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
